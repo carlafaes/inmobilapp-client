b537a1209177d000d5fe3f597d33740ff1b64b20
968578 <omar.echavarria56@gmail.com>
2022-02-16 12:59:21 -0500

    agregue provider a index con store

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store/store';
+
 
 import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  rootElement);
