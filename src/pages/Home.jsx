1f9f88c5f99d1ac9abc386a45f0fda629a79af26
968578 <omar.echavarria56@gmail.com>
2022-02-18 17:02:11 -0500

    cambio en home de 'ubication' a 'location'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,12 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
-import adminService from '../services/admin';
-import Landing from './Landingprueba';
-import Navbar from './Navbar';
-import NavFilterProperty from './Nav-filter';
-=======
 import propertyService from '../services/property';
 import Landing from '../componentes/Landingprueba';
 import Navbar from '../componentes/Navbar';
 import '../styles/Loading.css';
 import Load from '../Img/LOAD5gif.gif';
 import Footer from '../componentes/Footer';
->>>>>>> 9b13a84329d7102a83ba2963ee671b59c59d61f4
+import NavFilterProperty from '../componentes/Nav-filter'
+import Cards from '../componentes/cards-temporal';
 
 function Home() {
 	const [properties, setProperties] = useState([]);
@@ -32,28 +27,6 @@ function Home() {
 		);
 	}
 
-<<<<<<< HEAD
-  return (
-    <div>
-      <div>
-        <Landing />
-      </div>
-      <div>
-        <Navbar />
-        <NavFilterProperty/>
-      </div>
-      <h1>Hello world!</h1>
-      {admins.map((admin) => (
-        <div key={admin.name}>
-          <h1>{admin.name}</h1>
-          <p>{admin.password}</p>
-          <p>{admin.email}</p>
-        </div>
-      ))}
-
-    </div>
-  );
-=======
 	return (
 		<div>
 			<div>
@@ -61,12 +34,14 @@ function Home() {
 			</div>
 			<div>
 				<Navbar />
+        <NavFilterProperty/>
+        <Cards/>
 			</div>
 			<h1>Hello world!</h1>
 			{properties.map((propery) => (
 				<div key={propery.id}>
 					<h1>{propery.state}</h1>
-					<p>{propery.ubication.city}</p>
+					<p>{propery.location.city}</p>
 					<p>{propery.rentalPrice}</p>
 				</div>
 			))}
@@ -75,7 +50,6 @@ function Home() {
 			</div>
 		</div>
 	);
->>>>>>> 9b13a84329d7102a83ba2963ee671b59c59d61f4
 }
 
 export default Home;
