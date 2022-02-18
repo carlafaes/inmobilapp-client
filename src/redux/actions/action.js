39e1e8c95b9812ccdf4349b480e1a22ba78560ca
Jose Angarita <amjose09@gmail.com>
2022-02-18 17:32:48 -0500

    menu desplegable sin errores

--- src/redux/actions/action.js
+++
@@ -1,5 +0,0 @@
-export default function openMenuModal(){
-    return{
-        type:'OPEN_MENU_MODAL'
-    }
-}
--- src/redux/reducers/reducer-client.js
+++ src/redux/reducers/reducer-client.js
@@ -1,14 +1,9 @@
 const initialState = {
-  openModal:false
 };
 
 const clients = (state = initialState, action) => {
   switch (action.type) {
-    case 'OPEN_MENU_MODAL':
-      return{
-        ...state,
-        openModal: true
-      }
+    
     default: return state;
   }
 };
