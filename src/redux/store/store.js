4ef3df690c979f8c2d2ee4906c38eafb0fe04b20
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-21 12:26:40 -0500

    store fede

---
+++ src/redux/store/store.js
@@ -0,0 +1,10 @@
+import rootReducer from '../reducers/reducer-index';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEn = (typeof window !== "undefined" && 
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(rootReducer,composeEn(applyMiddleware(thunk)));
+
+export default store;
