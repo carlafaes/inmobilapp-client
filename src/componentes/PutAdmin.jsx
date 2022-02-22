6c2011330d274eea25cb2fdfd23a43fedac33ba2
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-22 17:22:05 -0500

    PutAdmin

--- src/componentes/PutAdmin.jsx
+++ src/componentes/PutAdmin.jsx
@@ -1,5 +1,19 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import adminService from "../services/admin";
+import Loading from "./Loading";
 
-export default function PutAdmin({id}) {
+export default function PutAdmin({ id }) {
+  const [admin, setAdmin] = useState({});
 
+  useEffect(() => {
+    adminService()
+      .getAdminID(id)
+      .then((data) => {
+        setAdmin(data);
+      });
+  }, [id]);
+
+  if (Object.keys(admin).length === 0) {
+    return <Loading />;
+  }
 }
