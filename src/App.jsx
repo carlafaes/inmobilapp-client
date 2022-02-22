f2a881243eab44c5cfd5b97be73c3d3ba9ae4831
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-21 22:22:26 -0500

    Form Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import PropertyDetails from './pages/PropertyDetails/PropertyDetails';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import PropertyDetails from "./pages/PropertyDetails/PropertyDetails";
 // import Landing from './pages/Landingprueba'
-import { LoginClient } from './componentes/LoginClient';
-import { RegisterClient } from './componentes/RegisterClient';
-import Home from './pages/Home';
-import CreateProperty from './componentes/CreateProperty'
+import { LoginClient } from "./componentes/LoginClient";
+import { RegisterClient } from "./componentes/RegisterClient";
+import Home from "./pages/Home";
+import CreateProperty from "./componentes/CreateProperty";
+import FormAdmin from "./pages/FormAdmin";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -13,9 +15,11 @@ function App() {
       {/* <Route path="/" element={<Landing />} /> */}
       <Route exact path="/" element={<Home />} />
       <Route exact path="/form" element={<CreateProperty />} />
-      <Route path="/login" element={<LoginClient/>}/>
-      <Route path='/register' element={<RegisterClient/>}/>
-      <Route exact path= "/property/:id" element={<PropertyDetails/>}/>
+      <Route path="/registerAdmin" element={<FormAdmin />} />
+      <Route path="/login" element={<LoginClient />} />
+      <Route path="/register" element={<RegisterClient />} />
+      <Route exact path="/property/:id" element={<PropertyDetails />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
---
+++ src/pages/FormAdmin.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Input from "../componentes/Input";
+import adminService from "../services/admin";
+import { validateFormAdmin } from "../utils/errorsFormAdmin";
+
+export default function FormAdmin() {
+  document.title = "InmobillApp | registerAdmin";
+  const initInput = {
+    name: "",
+    DNI: "",
+    address: "",
+    phone: "",
+    age: 0,
+  };
+
+  const [input, setInput] = useState(initInput);
+  const [error, setError] = useState({
+    name: true,
+    DNI: true,
+    address: true,
+    phone: true,
+    age: true,
+  });
+
+  const handleChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    setError(validateFormAdmin({ ...input, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isDone()) {
+      adminService.postAdmin(input).then(data => console.log(data));
+      setInput(initInput);
+    } else {
+      alert("No...");
+    }
+  };
+
+  const isDone = () => {
+    return !Object.values(error).reduce((pre, cur) => pre || cur, false);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundImage:
+          "url(https://www.xtrafondos.com/descargar.php?id=3474&resolucion=2560x1440)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <form
+        style={{ maxWidth: "80%", display: "grid" }}
+        onSubmit={handleSubmit}
+      >
+        <Input
+          error={error.name}
+          placeholder={"Name"}
+          type={"text"}
+          name="name"
+          handleChange={handleChange}
+        />
+        <Input
+          error={error.DNI}
+          placeholder={"DNI"}
+          type={"text"}
+          name="DNI"
+          handleChange={handleChange}
+        />
+        <Input
+          error={error.address}
+          placeholder={"Address"}
+          type={"text"}
+          name="address"
+          handleChange={handleChange}
+        />
+        <Input
+          error={error.phone}
+          placeholder={"Phone"}
+          type={"text"}
+          name="phone"
+          handleChange={handleChange}
+        />
+        <Input
+          error={error.age}
+          placeholder={"Age"}
+          type={"number"}
+          name="age"
+          handleChange={handleChange}
+        />
+        <button onClick={handleSubmit}>send</button>
+      </form>
+    </div>
+  );
+}
