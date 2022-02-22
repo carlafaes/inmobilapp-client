35e05c1affcb516d9bf89bb23d3f6c5591877fe2
carlafaes <carlafaes18@hotmail.com>
2022-02-22 13:30:42 -0300

    Cambios rutas reviews,reducer reviews,action reviews,componente scoreMax

--- src/componentes/ScoreMax.jsx
+++ src/componentes/ScoreMax.jsx
@@ -2,24 +2,42 @@ import React from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import { useEffect } from 'react';
 import { getScore,orderByScore } from '../redux/actions/action-properties-score';
+import reviewsService from '../services/reviews';
+import {filterRevies,getAllReviews,getScoreReviews} from '../redux/actions/actions-reviews'
 
 export default function ScoreMax(){
     const dispatch= useDispatch();
-    const stateScoreMax= useSelector((state)=>state.filteredScore);
+    const reviews = useSelector((state) => state.reviews.propertiesScore);
+  
+
+    
+  useEffect(() => {
+    reviewsService.getPropertiesReviews().then((data) => {
+      return dispatch(getAllReviews(data));
+    }).then(() => {
+      return dispatch(filterRevies())
+    }).then(() => {
+      dispatch(getScoreReviews());
+    });
+  }, []);
+
+  console.log(reviews);
+  
+    // const stateScoreMax= useSelector((state)=>state.filteredScore);
     
 
-    useEffect(()=>{
-      dispatch(getScore()) 
+    // useEffect(()=>{
+    //   dispatch(getScore()) 
 
-    },[]);
+    // },[]);
 
-    useEffect(()=>{
-        // if(stateScoreMax){
-        //     dispatch(orderByScore(stateScoreMax))
-        // }
-        dispatch(orderByScore())
-    },[])
-    console.log(stateScoreMax,'stateScoreMax')
+    // useEffect(()=>{
+    //     // if(stateScoreMax){
+    //     //     dispatch(orderByScore(stateScoreMax))
+    //     // }
+    //     dispatch(orderByScore())
+    // },[])
+    
     return(
         <div>
             <h1>score</h1>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -5,20 +5,6 @@ import thunk from 'redux-thunk';
 const composeEn = (typeof window !== "undefined" && 
 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-<<<<<<< HEAD
-/* const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-import { createStore,applyMiddleware } from 'redux';
-import rootReducer from '../reducers/reducer-index';
-import thunk from 'redux-thunk' */
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
-=======
 const store = createStore(rootReducer,composeEn(applyMiddleware(thunk)));
->>>>>>> 12f52f5676c5f80abaf82f3961d00eb18e4ec4cb
 
 export default store;
