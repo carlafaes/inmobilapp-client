cad175571d11eb8818d26f6b3860f388eebc85e9
carlafaes <carlafaes18@hotmail.com>
2022-02-23 19:22:50 -0300

    Arregle ubicacion de paginado y de landing(arrow,logo,slogan)

--- src/componentes/Landingprueba.jsx
+++ src/componentes/Landingprueba.jsx
@@ -5,6 +5,8 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link as Scroll } from 'react-scroll';
 import Logo from '../Img/logo.png';
 import { Navigate } from 'react-router-dom';
+// import '../styles/Landing.css'
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,8 +19,18 @@ const useStyles = makeStyles((theme) => ({
     zIndex: '10',
   },
   imgLogo: {
-    marginLeft: '28%',
+    marginLeft: '210px',
     marginTop: '2%',
+    '@media (max-width:575px)' : {
+      marginLeft:'100px',
+      width:'300px',
+      marginTop:'-10%',
+    },
+    '@media (max-width:1200px)' : {
+      width: '400px',
+      marginLeft:'170px',
+      height:'300px',
+    }
   },
   slogan: {
     marginLeft: '36%',
@@ -27,14 +39,36 @@ const useStyles = makeStyles((theme) => ({
     color:'#0d0d0d',
     fontFamily: 'Times New Roman',
     textShadow: '#faa222 1px 1px',
+  '@media (max-width:575px)' : {
+    width: '50%',
+    fontSize:'20px',
+    marginLeft: '26%',
+  },
+  '@media (max-width:1200px)' : {
+    width: '400px',
+    marginLeft:'170px',
+    height:'80px',
+  }
   },
   arrow: {
+    width:'100px',
     color: '#faa222',
-    fontSize: '4rem',
-    marginLeft: '185vh',
+    // fontSize: '40rem',
+     marginLeft: '385px',
     marginTop: '-5%',
     position: 'absolute',
     zIndex: '10',
+    height:'80px',
+    '@media (max-width:575px)' : {
+      width: '100px',
+      marginLeft:'170px',
+      height:'40px',
+    },
+    '@media (max-width:1200px)' : {
+      width: '100px',
+      marginLeft:'270px',
+      height:'40px',
+    }
   },
 }));
 
@@ -56,13 +90,15 @@ export default function Landing() {
         {...(checked ? { timeout: 1000 } : {})}
         collapsedSize={50}
       >
-        <img className={classes.imgLogo} src={Logo} alt="imglogo" width={650} height={450} />
-        <h3 className={classes.slogan}>Innovación inmobiliaria</h3>
+        <img className={classes.imgLogo}  src={Logo} alt="imglogo" width={650} height={450} />
+        <h3 className={classes.slogan} >Innovación inmobiliaria</h3>
         <CssBaseline />
         <div>
           <Scroll to="navbar" smooth>
             <IconButton>
+              <div className='arrow_landing'>
               <ExpandMoreIcon className={classes.arrow} />
+              </div>
             </IconButton>
           </Scroll>
         </div>
