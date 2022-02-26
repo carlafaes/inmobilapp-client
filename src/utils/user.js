368b0aeb1068acb21c32c8fcad1db712153fbbd8
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-26 09:32:23 -0500

    NavBarAdmin

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,3 +1,7 @@
 export const getUserForLocalStorage = () => {
   return JSON.parse(localStorage.getItem("loggedUser"));
 };
+
+export const logaoutCurrentUserForLocalStorage = () => {
+  localStorage.removeItem("loggedUser");
+};
