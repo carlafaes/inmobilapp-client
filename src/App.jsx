d9fce81dc0b2c0848fb8bcc40c2a95f86166c117
Dairo01001 <garcianaranjodairo@gmail.com>
2022-02-26 09:06:28 -0500

    view Admin route

--- src/App.jsx
+++ src/App.jsx
@@ -14,34 +14,31 @@ import ViewAdmin from "./pages/ViewAdmin";
 import { ToastContainer } from "react-toastify";
 import MaquetaForm from "./componentes/MaquetaForm";
 import QuienesSomos from "./componentes/QuienesSomos";
-import {ClientInterface} from "./componentes/ClientInterface/ClientInterface";
-import {Toaster} from 'react-hot-toast'
+import { ClientInterface } from "./componentes/ClientInterface/ClientInterface";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-        return (
-                <>
-                        <Routes>
-                                {/* <Route path="/" element={<Landing />} /> */}
-                                <Route path="*" element={<PageNotFound />} />
-                                <Route path="/" element={<Home />} />
-                                <Route path="/property/:id" element={<PropertyDetails />} />
-                                <Route path="/register" element={<RegisterClient />} />
-                                <Route path="/viewClient" element={<ClientInterface />} />
-                                <Route path="/viewAdmin/:id" element={<ViewAdmin />} />
-                                <Route path="/registerAdmin" element={<FormAdmin />} />
-                                <Route exact path="/form" element={<CreateProperty />} />
-                                <Route path="/login" element={<LoginBeta />} />
-                                <Route path="/agents/:id" element={<AgentDetail />} />
-                                <Route path="/create-property" element={<CreatePropertyForm />} />
-                                <Route path="/quienes" element={<QuienesSomos />} />
-                                <Route path="/maquetaform" element={<MaquetaForm />} />
-                        </Routes>
-                        <Toaster
-                        position="top-center"
-                        reverseOrder={false}
-                        />
-                        <ToastContainer />
-                </>
-        );
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/register" element={<RegisterClient />} />
+        <Route path="/viewClient" element={<ClientInterface />} />
+        <Route path="/viewAdmin" element={<ViewAdmin />} />
+        <Route path="/registerAdmin" element={<FormAdmin />} />
+        <Route exact path="/form" element={<CreateProperty />} />
+        <Route path="/login" element={<LoginBeta />} />
+        <Route path="/agents/:id" element={<AgentDetail />} />
+        <Route path="/create-property" element={<CreatePropertyForm />} />
+        <Route path="/quienes" element={<QuienesSomos />} />
+        <Route path="/maquetaform" element={<MaquetaForm />} />
+      </Routes>
+      <Toaster position="top-center" reverseOrder={false} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
