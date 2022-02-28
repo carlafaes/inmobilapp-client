f0a2f10cc5ffab4e987b8bb506117811f45a1766
Brayan Baquero <babaquero3@misena.edu.co>
2022-02-28 10:51:38 -0500

    Conflicto en client services solucionado

--- src/services/client.js
+++ src/services/client.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 export const updateInfo = async (infoToUpdate, token) => {
   const URL = "/api/clients";
-=======
-const updateInfo = async (infoToUpdate, clientID, token) => {
-  const URL = `/api/clients/${clientID}`;
->>>>>>> 7daaddceeecf3faaf1db014a69f16a00ec5d254d
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -21,7 +16,6 @@ const updateInfo = async (infoToUpdate, clientID, token) => {
   }
 };
 
-<<<<<<< HEAD
 export const getClientInfo = async (clientID) => {
   const URL = `/api/clients/${clientID}`;
   try {
@@ -38,24 +32,3 @@ const clientService = {
 };
 
 export default clientService;
-=======
-const getclientInfo=async (clientID,token)=>{
-  const URL=`/api/clients/${clientID}`;
-  const config={
-    headers: {
-      Authorization:`Bearer ${token}`
-    }
-  };
-  try{
-    const response=await axios.get(URL,config);
-    return response.data
-  }catch(error){
-    console.log(error);
-  }
-}
-const services={
-  updateInfo,
-  getclientInfo
-}
-export default services;
->>>>>>> 7daaddceeecf3faaf1db014a69f16a00ec5d254d
