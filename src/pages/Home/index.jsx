b5a5860ad6f550dbd51effcb006c085395474da9
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-03 14:26:11 -0500

    Theme

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,6 @@ import Loading from "../../componentes/Loading";
 import Footer from "../../componentes/Footer";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { Switch } from "@material-ui/core";
 import ScoreMax from "../../componentes/ScoreMax";
 import { ListCard } from "../../componentes/ListCard";
@@ -14,14 +13,15 @@ import FilterProperties from "../../componentes/FilterProperties";
 import { setAllProperties } from "../../redux/actions/actionsProperties";
 import { paginate } from "../../utils/paginate";
 import styled from "./Home.module.css";
+import { changeTheme, getTheme } from "../../utils/theme";
 
 function Home() {
   const properties = useSelector(
     (state) => state.reducerProperties.filteredProperties
   );
   const dispatch = useDispatch();
-  const [darkMode, setDarkMode] = useState(false);
   const [pageNumber, setPageNumber] = useState(0);
+  const [darkMode, setDarkMode] = useState(getTheme() === "dark");
 
   const nextPage = () => {
     setPageNumber(pageNumber + 1);
@@ -42,52 +42,47 @@ function Home() {
     <Loading />;
   }
 
-  const theme = createTheme({
-    palette: {
-      type: darkMode ? "dark" : "light",
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <div>
       <div>
-        <div>
-          <Landing />
-        </div>
-        <div>
-          <Navbar />
-          <div className={styled.switch_home}>
-            <h2>
-              🔆
-              <Switch
-                checked={darkMode}
-                onChange={() => setDarkMode(!darkMode)}
-                color="primary"
-              />
-              🌙
-            </h2>
-          </div>
-          <br />
-
-          <ScoreMax />
-          <FilterProperties />
-        </div>
-        <div className={styled.containerPaginate}>
-          <button onClick={previusPage} className={styled.buttonPaginate}>
-            Anterior
-          </button>
-          <button onClick={nextPage} className={styled.buttonPaginate}>
-            Siguiente
-          </button>
-        </div>
-        <ListCard
-          properties={paginate(properties, pageNumber, propertiesPerPage)}
-        />
-        <div>
-          <Footer />
-        </div>
+        <Landing />
       </div>
-    </ThemeProvider>
+      <div>
+        <Navbar />
+        <div className={styled.switch_home}>
+          <h2>
+            🔆
+            <Switch
+              checked={darkMode}
+              onChange={() => {
+                changeTheme();
+                setDarkMode(darkMode);
+              }}
+              color="primary"
+            />
+            🌙
+          </h2>
+        </div>
+        <br />
+
+        <ScoreMax />
+        <FilterProperties />
+      </div>
+      <div className={styled.containerPaginate}>
+        <button onClick={previusPage} className={styled.buttonPaginate}>
+          Anterior
+        </button>
+        <button onClick={nextPage} className={styled.buttonPaginate}>
+          Siguiente
+        </button>
+      </div>
+      <ListCard
+        properties={paginate(properties, pageNumber, propertiesPerPage)}
+      />
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
