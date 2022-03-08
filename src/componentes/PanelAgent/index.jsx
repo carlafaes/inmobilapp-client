a4a0d9dba80ed845f02b74cd490d2dd21d1446f8
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-07 21:12:41 -0500

    template

--- src/componentes/PanelAgent/index.jsx
+++ src/componentes/PanelAgent/index.jsx
@@ -1,13 +1,30 @@
 import { IconButton } from "@mui/material";
+import React, { useState } from "react";
 import { BsFillPlusSquareFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import CardPropertyAgent from "../CardPropertyAgent";
+import axios from "axios";
+import { msg } from "../../templates/ClientSuscribe";
 
 import classes from "./PanelAgent.module.css";
 
 export default function PanelAgent({ agent }) {
-
   const navigate = useNavigate();
+
+  const seAlquiloPropiedad = (name, email, property) => {
+    console.log(property);
+    axios.post("/api/sendemail", {
+      email,
+      name,
+      template: msg(name, email, property),
+    });
+  };
+
+  const [input, setInput] = useState({
+    name: "Dairo",
+    email: "garcianaranjodairo@gmail.com",
+    agentID: 123,
+  });
 
   return (
     <div className={classes.container}>
@@ -15,6 +32,17 @@ export default function PanelAgent({ agent }) {
         <IconButton onClick={() => navigate("/create-property")}>
           <BsFillPlusSquareFill />
         </IconButton>
+        <button
+          onClick={() =>
+            seAlquiloPropiedad(
+              "Carla",
+              "garcianaranjodairo@gmail.com",
+              agent.properties[0]
+            )
+          }
+        >
+          enviar
+        </button>
       </div>
       <div className={classes.content}>
         {agent.properties.map((property) => {
