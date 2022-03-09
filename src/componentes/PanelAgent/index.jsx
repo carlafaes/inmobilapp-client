bd36320ac710f560f8f2c198a6e0c76ab3605a74
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-09 13:10:19 -0500

    Conexiones

--- src/componentes/PanelAgent/index.jsx
+++ src/componentes/PanelAgent/index.jsx
@@ -1,39 +1,13 @@
 import { IconButton } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import { BsFillPlusSquareFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import CardPropertyAgent from "../CardPropertyAgent";
-import axios from "axios";
-import { msg } from "../../templates/ClientSuscribe";
+
 import classes from "./PanelAgent.module.css";
-import Modal from './Modal/Modal'
 
 export default function PanelAgent({ agent }) {
   const navigate = useNavigate();
-  const [send, setSend]=useState(false);
-  
-
-  
-  
-  const seAlquiloPropiedad = (name, email, property) => {
-    console.log(property);
-    axios.post("/api/sendemail", {
-      email,
-      name,
-      template: msg(name, email, property),
-    });
-    setTimeout(() => {
-      setSend(true)
-    }, 1000);
-    
-  };
-
-
-  const [input, setInput] = useState({
-    name: "Dairo",
-    email: "garcianaranjodairo@gmail.com",
-    agentID: 123,
-  });
 
   return (
     <div className={classes.container}>
@@ -41,28 +15,12 @@ export default function PanelAgent({ agent }) {
         <IconButton onClick={() => navigate("/create-property")}>
           <BsFillPlusSquareFill />
         </IconButton>
-        <button
-          onClick={() =>
-            seAlquiloPropiedad(
-              "Car",
-              "carlaf2193@gmail.com",
-              agent.properties[0]
-            )
-          }
-        >
-          
-          enviar
-        </button>
       </div>
       <div className={classes.content}>
         {agent.properties.map((property) => {
           return <CardPropertyAgent key={property.id} {...property} />;
         })}
       </div>
-      {send && <Modal send={setSend}/>}
-     
     </div>
   );
-  }
-
-  
+}
--- src/pages/AssignProperty/index.jsx
+++ src/pages/AssignProperty/index.jsx
@@ -4,15 +4,20 @@ import agentService from "../../services/agent";
 import { getUserForLocalStorage } from "../../utils/user";
 import swal from "sweetalert";
 import { notifyWelcome } from "../../utils/notifications";
-import "./assignProperty.css";
 import NavBarAgent from "../../componentes/NavBarAgent";
 import Footer from "../../componentes/Footer";
 import Loading from "../../componentes/Loading";
+import Modal from "./Modal/Modal";
 import { useNavigate } from "react-router-dom";
+import { msg } from "../../templates/ClientSuscribe";
+import axios from "axios";
+
+import styled from "./AssingProperty.module.css";
 
 export default function AssignProperty() {
   const { clientID, propertyID } = useParams();
   const [agentDetail, setAgentDetail] = useState(null);
+  const [send, setSend] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,17 +32,35 @@ export default function AssignProperty() {
     });
   }, []);
 
+  const seAlquiloPropiedad = (name, email, property) => {
+    axios.post("/api/sendemail", {
+      email,
+      name,
+      template: msg(name, property),
+    });
+    setTimeout(() => {
+      setSend(true);
+    }, 1000);
+  };
+
   const asignarPropiedad = () =>
     agentService
       .assignProperty(clientID, propertyID, agentDetail.token)
-      .then(() => swal("Propiedad asignada exitosamente"))
-      .catch(() => swal("Ha ocurrido un error :("));
+      .then((data) => {
+        seAlquiloPropiedad(data.name, data.email, data.propertyID);
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("Ha ocurrido un error :(");
+      });
 
   const eliminarPropiedad = () =>
     agentService
       .delClientProperty(clientID, true, agentDetail.token)
       .then(() => swal("Se eliminó la propiedad del cliente"))
-      .catch(() => swal("Ha ocurrido un error :("));
+      .catch((err) => {
+        swal("Ha ocurrido un error :(");
+      });
 
   if (!agentDetail) {
     return <Loading />;
@@ -49,8 +72,11 @@ export default function AssignProperty() {
       <p>
         {clientID}, {propertyID}
       </p>
-      <button onClick={asignarPropiedad}>Asignar propiedad al cliente</button>
-      <button onClick={eliminarPropiedad}>
+      <button className={styled.button} onClick={asignarPropiedad}>
+        Asignar propiedad al cliente
+      </button>
+      {send && <Modal send={setSend} />}
+      <button className={styled.button} onClick={eliminarPropiedad}>
         Eliminar propiedad del cliente
       </button>
       {/* <Footer /> */}
